d60aeec370d97bed9e670d5c4e888ef1477f7dd7
Chrono-10 <104217790+Chrono-10@users.noreply.github.com>
2024-05-23 13:04:13 +0200

    Update script.js
    computer choice added

--- script.js
+++ script.js
@@ -1,35 +1,36 @@
-
-
-
-
-
+//computer reponse
+function computerPlay() {
+  let choice = ['Rock', 'Paper', 'Scissors'];
+  let reponse_num = Math.floor(Math.random() * 3);
+  let reponse_choice = choice[reponse_num];
+  return reponse_choice;
+}
 
 //single round play
 
-function playRound( playerSelection,computerSelection){
+function playRound(playerSelection, computerSelection) {
 
-  const computerChoice = computerSelection.toLowerCase();
- const playerChoice =  playerSelection.toLowerCase();
+  let computerChoice = computerSelection.toLowerCase();
+  let playerChoice = playerSelection.toLowerCase();
 
   if ((playerChoice === 'rock' && computerChoice === 'scissors') ||
-  (playerChoice === 'scissors' && computerChoice === 'paper') ||
-  (playerChoice === 'paper' && computerChoice === 'rock')) 
-  {
-   return `you win because ${playerChoice} beats ${computerChoice}`
+    (playerChoice === 'scissors' && computerChoice === 'paper') ||
+    (playerChoice === 'paper' && computerChoice === 'rock')) {
+    return `you win because ${playerChoice} beats ${computerChoice}`
   }
 
-  else if(playerChoice===computerChoice)
- 
-    {
-     return "its a tie"
-    }
+  else if (playerChoice === computerChoice) {
+    return "its a tie"
+  }
 
-    else{
-     return `you lose because ${computerChoice} beats ${playerChoice}`
-    }
- }
+  else {
+    return `you lose because ${computerChoice} beats ${playerChoice}`
+  }
+}
 
- const playerSelection = 'Rock';
- const computerSelection = computerPlay();
 
- console.log(playRound(playerSelection,computerSelection));
+
+let playerSelection = 'Rock';
+let computerSelection = computerPlay();
+
+console.log(playRound(playerSelection, computerSelection));
